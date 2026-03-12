b0083d1f83e9defc2f7fa65b06079fa7fe94eb3f
agentP <learnhacksandtricks@gmail.com>
2026-03-12 00:44:42 -0700

    ui ish

---
+++ popup.js
@@ -0,0 +1,5 @@
+document.getElementById("add-recipe-btn").addEventListener("click", () => {
+  // Placeholder for future logic
+  console.log("Add to Recipe button clicked!");
+  alert("This will eventually add the recipe to your list!");
+});
